7c7c3d2f41747401238647dbfbe81c24489dcdc2
ciceropzr <ciceropzr@gmail.com>
2018-01-30 17:27:21 -0200

    aula3

--- biscesto.py
+++ biscesto.py
@@ -1,7 +1,7 @@
 x = int(input('Insira o ano => '))
 
 if x % 4 == 0:
-  print('Ano Bicesto')
+  print('Ano Bicesto.\nEsse ano tem 366 dias!')
 
 else:
-    print('Ano normal')
+    print('Ano com 365 dias!')
